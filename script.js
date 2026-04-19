aef4b5647b40867796b97f71d63ed93f58506d1f
anni-cool <geierstangera0837@my.sduhsd.net>
2026-04-19 19:10:31 +0000

    I worked on my javascript alot! I included a list, a parameter, a function, and connected by buttons together.

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-
-
-//batter
-let batterPrice = 0;
-document.getElementById("gluten-free").addEventListener("checked, function() {
-    batterPrice = 5.25;
-    num= document.getElementById("gluten-free").value;
-    console.log(num);
-}
-//fillings 
-
-//toppings 
-
-//sides 
+//items this is the list 
+let items = [
+    {id: "gluten-free", price: 5.25},
+    {id: "whole-wheat", price: 4.50},
+    {id: "buttermilk", price: 4.50},
+    {id: "blueberries", price: 0.50},
+    {id: "chocolate-chip", price: 0.50},
+    {id: "banana", price: 0.50},
+    {id: "maple-syrup", price: 0.50},
+    {id: "butter", price: 0.50},
+    {id: "whipped-cream", price: 0.75},
+    {id: "eggs", price: 2.00},
+    {id: "bacon", price: 2.50},
+    {id: "sausage", price: 2.00},
+]
 
 //total price 
-totalPrice= batterPrice 
+function calculateTotalPrice(items) {
+    let totalPrice = 0
+    for (let i = 0; i < items.length; i++) {
+        let item = items[i];
+        let checkbox = document.getElementById(item.id)
+        if (checkbox && checkbox.checked) {
+            totalPrice += item.price;
+        }
+    }
+return totalPrice; 
+}
+    
+
+
+//calculate button 
+document.getElementById("btnCalc").addEventListener("click", function(){
+let totalPrice = calculateTotalPrice(items);
 document.getElementById("final-cost").textContent = totalPrice.toFixed(2);
+}); 
